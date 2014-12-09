1b9444b01f377adaf04827073153c467b11a021f
shilpee <silpinaik11@gmail.com>
2014-12-09 15:49:31 +0530

    This is my first commit

---
+++ gittask/src/gittask/main_class.java
@@ -0,0 +1,10 @@
+package gittask;
+
+public class main_class {
+
+	public static void main(String[] args) {
+		System.out.println("printing something");
+
+	}
+
+}
---
+++ gittask/src/gittask/subclass.java
@@ -0,0 +1,7 @@
+package gittask;
+
+public class subclass {
+public void subclass_print(){
+	System.out.println("printing the output");
+}
+}
