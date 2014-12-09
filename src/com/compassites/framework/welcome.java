6698b68310d034be0fe7c89db2e7aae6d965bbbf
shilpee <silpinaik11@gmail.com>
2014-12-09 13:31:46 +0530

    My first commit

---
+++ src/com/compassites/framework/welcome.java
@@ -0,0 +1,12 @@
+package com.compassites.framework;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		System.out.println("welcome screen");
+		welcomeperformancetesting wp=new welcomeperformancetesting();
+		wp.displaywelcomeMessage();
+
+	}
+
+}
---
+++ src/com/compassites/framework/welcomeperformancetesting.java
@@ -0,0 +1,10 @@
+package com.compassites.framework;
+
+public class welcomeperformancetesting {
+
+	public void displaywelcomeMessage(){
+		System.out.println("welcome performance testing");
+
+	}
+
+}
