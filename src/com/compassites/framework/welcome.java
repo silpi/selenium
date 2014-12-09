df93c7c55587317d5d3a4b79111afc0d4d181576
shilpee <silpinaik11@gmail.com>
2014-12-09 13:56:26 +0530

    my second commit

--- src/com/compassites/framework/welcome.java
+++ src/com/compassites/framework/welcome.java
@@ -3,7 +3,8 @@ package com.compassites.framework;
 public class welcome {
 
 	public static void main(String[] args) {
-		System.out.println("welcome screen");
+		System.out.println("welcome screen firstmessage");
+		System.out.println("welcome screen lastmessage");
 		welcomeperformancetesting wp=new welcomeperformancetesting();
 		wp.displaywelcomeMessage();
 
